6f59463dd91726cc0d70e905e2a88ddc2ef6c190
jsc5023 <kir5121@naver.com>
2024-07-05 23:17:14 +0900

    [feat] - UserAccountRepository 추가

---
+++ src/main/java/com/example/loan/repository/UserAccountRepository.java
@@ -0,0 +1,8 @@
+package com.example.loan.repository;
+
+import com.example.loan.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+
+}
