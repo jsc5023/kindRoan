a832a8fc8e139e11991fa080688c976f67f383b6
jsc5023 <kir5121@naver.com>
2024-07-08 12:20:33 +0900

    [feat] - Security에서 로그인 ProccessUrl 및 defaultSucessUrl 변경하여 로그인이 작동될 수 있도록 변경

--- src/main/java/com/example/loan/config/SecurityConfig.java
+++ src/main/java/com/example/loan/config/SecurityConfig.java
@@ -4,10 +4,14 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,35 +22,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests((auth) -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers(HttpMethod.GET, "/security/*", "/","/index", "/js/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/security/*", "/","/index", "/css/**", "/images/**", "/js/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin((formLogin) -> formLogin.loginPage("/security/login")
-                .loginProcessingUrl("/security/loginProc")
-                        .defaultSuccessUrl("/"))
+                        .loginProcessingUrl("/security/loginProc")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
                 .logout((logout) -> logout.logoutSuccessUrl("/"))
                 .build();
-
-//
-//            .formLogin((formLogin) -> formLogin.loginPage("/security/login")
-//                .loginProcessingUrl("/security/loginProc")
-//                .defaultSuccessUrl("/index"))
-//                .logout((logout) ->
-//                        logout.logoutSuccessUrl("/index"))
-
-//        return http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeRequests((authz) -> authz
-//                        .requestMatchers("/index", "/css/**", "/images/**", "/js/**").permitAll()
-//                        .anyRequest().authenticated())
-//                .formLogin((formLogin) ->
-//                        formLogin.loginPage("security/login.html")
-//                                .defaultSuccessUrl(".")
-//                ).logout((logout) ->
-//                        logout.logoutSuccessUrl("security/login.html")
-//                                .invalidateHttpSession(true)
-//                ).build();
-
     }
 }
