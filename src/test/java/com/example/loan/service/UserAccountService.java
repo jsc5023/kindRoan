93e1c7b329ff00e04cb4cce1ca0ac18c696ae280
jsc5023 <kir5121@naver.com>
2024-07-21 21:00:19 +0900

    [feat] - UserAccountServies Test 기능 추가

---
+++ src/test/java/com/example/loan/service/UserAccountService.java
@@ -0,0 +1,76 @@
+package com.example.loan.service;
+
+import com.example.loan.domain.UserAccount;
+import com.example.loan.dto.UserAccountDto;
+import com.example.loan.repository.UserAccountRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@DisplayName("회원 관련 로직")
+@ExtendWith(MockitoExtension.class)
+public class UserAccountService {
+
+    @InjectMocks private UserAccountService sut;
+
+    @Mock private UserAccountRepository userAccountRepository;
+
+    @DisplayName("회원 정보를 입력하면, 새로운 회원 정보를 저장하여 가입시키고 해당 회원을 리턴한다.")
+    @Test
+    void givenUserParams_whenSaving_thenSavesUserAccount() {
+        // Given
+        UserAccount userAccount = createUserAccount("jsc");
+        UserAccount saveUserAccount = createSigningUpUserAccount("jsc");
+        given(userAccountRepository.save(userAccount)).willReturn(saveUserAccount);
+
+        // When
+//        UserAccountDto result = sut.saveUser(
+//                userAccount.getUserId(),
+//                userAccount.getUserPassword(),
+//                userAccount.getEmail(),
+//                userAccount.getNickname(),
+//                userAccount.getMemo()
+//        );
+//
+//        // Then
+//        assertThat(result)
+//                .hasFieldOrPropertyWithValue("userId", userAccount.getUserId())
+//                .hasFieldOrPropertyWithValue("userPassword", userAccount.getUserPassword())
+//                .hasFieldOrPropertyWithValue("email", userAccount.getEmail())
+//                .hasFieldOrPropertyWithValue("nickname", userAccount.getNickname())
+//                .hasFieldOrPropertyWithValue("memo", userAccount.getMemo())
+//                .hasFieldOrPropertyWithValue("createdBy", userAccount.getUserId())
+//                .hasFieldOrPropertyWithValue("modifiedBy", userAccount.getUserId());
+//        then(userAccountRepository).should().save(userAccount);
+    }
+
+    private UserAccount createUserAccount(String username) {
+        return createUserAccount(username, null);
+    }
+
+    private UserAccount createUserAccount(String userId, String createdBy) {
+        return UserAccount.of(
+                userId,
+                "password",
+                "e@mail.com",
+                "nickname",
+                "memo",
+                LocalDateTime.now(),
+                'M',
+                createdBy
+        );
+    }
+
+    private UserAccount createSigningUpUserAccount(String username) {
+        return createUserAccount(username, username);
+    }
+}
