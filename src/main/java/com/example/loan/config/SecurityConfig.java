3a94ca262d749377f039c1cbadf989b8be5ff371
jsc5023 <kir5121@naver.com>
2024-07-25 13:21:08 +0900

    [feat] - login 실패시 프로세서 및 Spring Security Config 설정 변경

--- src/main/java/com/example/loan/config/SecurityConfig.java
+++ src/main/java/com/example/loan/config/SecurityConfig.java
@@ -1,23 +1,24 @@
 package com.example.loan.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@RequiredArgsConstructor
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    private final AuthenticationFailureHandler customFailureHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,6 +30,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .formLogin((formLogin) -> formLogin.loginPage("/security/login")
                         .loginProcessingUrl("/security/loginProc")
+                        .failureHandler(customFailureHandler)
                         .defaultSuccessUrl("/", true)
                         .permitAll()
                 )
