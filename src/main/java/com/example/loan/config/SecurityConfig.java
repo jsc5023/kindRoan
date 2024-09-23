7bd1fed7e5417b02bfc0e3180ce9bb47674353f2
SeongCheol Jung <kir5121@naver.com>
2024-09-23 22:56:48 +0900

    [feat] - 패스워드 인코더 추가

--- src/main/java/com/example/loan/config/SecurityConfig.java
+++ src/main/java/com/example/loan/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.example.loan.config;
 
+import com.example.loan.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
@@ -36,5 +43,10 @@ public class SecurityConfig  {
                 )
                 .logout((logout) -> logout.logoutSuccessUrl("/"))
                 .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
